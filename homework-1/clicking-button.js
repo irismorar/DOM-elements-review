1c4a3360006ac9c00ec431002747064134966c1e
Iris Morar <morariris@gmail.com>
2024-10-15 11:09:16 +0300

    improve buttons program

--- homework-1/clicking-button.js
+++ homework-1/clicking-button.js
@@ -1,8 +1,8 @@
-const A_square = document.getElementById("squareA")
-const B_square = document.getElementById("squareB")
-const C_square = document.getElementById("squareC")
+const A_square = document.querySelector("button.A")
+const B_square = document.querySelector("button.B")
+const C_square = document.querySelector("button.C")
 
-document.body.addEventListener("click", function(event){
+function handleClick(event){
   if (event.target === A_square) {
     alert("You've clicked on A")
   }
@@ -14,4 +14,6 @@ document.body.addEventListener("click", function(event){
   if (event.target === C_square) {
     alert("you've clicked on C")
   }
-})
+}
+
+document.body.addEventListener("click", handleClick)
